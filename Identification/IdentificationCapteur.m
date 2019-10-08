74a3cf32bf41bb6ace1e9a1917f5bb2718e70fb2
Vincent <guev3102@usherbrooke.ca>
2019-10-08 19:21:03 -0400

    Reorganisation des fichiers

---
+++ Identification/IdentificationCapteur.m
@@ -0,0 +1,6 @@
+%% Identification de la formule des capteurs
+clear all
+close all
+clc
+
+load("..\DonneesExperiemntales\capteur.mat",'all_imagenames','mapping')
---
+++ ModeleNonLineaire/Simulation/run_bancessai.m
@@ -0,0 +1,42 @@
+close all
+clear all
+clc
+
+% Position à l'équilibre de la sphère (pour tests statiques)
+sig = 1.0;         % Présence (1) ou non (0) de la sphère
+xSeq = 0.000;      % Position x de la sphère à l'équilibre en metres
+ySeq = 0.000;      % Position y de la sphère à l'équilibre en metres
+
+%Point d'opération choisi pour la plaque
+Axeq = 0;               %en degres
+Ayeq = 0;               %en degres
+Pzeq = .015;            %en metres
+
+%Exemple de trajectoire
+t_des     = [0:1:8]'*5;
+
+x_des     = [t_des, [0 0 0.5 1  0 -1 0 1 0]'*0.05];
+y_des     = [t_des, [0 0 0 0 -1  0 1 0 0]'*0.05];
+z_des     = [t_des, [1 1 1 1  1  1 1 1 1]'*.015];
+tfin = 50;
+
+%initialisation
+bancEssaiConstantes
+%bancessai_ini  %faites tous vos calculs de modele ici
+
+%Calcul des compensateurs
+%iniCTL_ver4    %Calculez vos compensateurs ici
+
+%simulation
+open_system('DYNctl_ver4_etud_obfusc')
+set_param('DYNctl_ver4_etud_obfusc','AlgebraicLoopSolver','LineSearch')
+sim('DYNctl_ver4_etud_obfusc')
+
+%% affichage
+titre = ["Ax";"Ay";"Pz";"Wx";"Wy";"Vz";"Px";"Py";"Vx";"Vy";"Ia";"Ib";"Ic";"zA";"zB";"zC";"zD";"zE";"zF";"Fa";"Fb";"Fc";"Va";"Vb";"Vc"];
+for i = 1:1:25
+    figure
+    plot(tsim, ynonlineaire(:,i));title(titre(i));
+end
+%trajectoires
+
