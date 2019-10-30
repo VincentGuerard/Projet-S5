0ca0e2ac94e39453ab4bc369bcb72cee56ac5740
Vincent <guev3102@usherbrooke.ca>
2019-10-30 16:13:26 -0400

    Ajout des blocs qui calculent la distance

--- ModeleNonLineaire/Simulation/run_bancessai.m
+++ ModeleNonLineaire/Simulation/run_bancessai.m
@@ -36,12 +36,6 @@ set_param('DYNctl_ver4_etud_obfusc','AlgebraicLoopSolver','LineSearch')
 sim('DYNctl_ver4_etud_obfusc')
 
 %% affichage
-% titre = ["Ax";"Ay";"Pz";"Wx";"Wy";"Vz";"Px";"Py";"Vx";"Vy";"Ia";"Ib";"Ic";"zA";"zB";"zC";"zD";"zE";"zF";"Fa";"Fb";"Fc";"Va";"Vb";"Vc"];
-% for i = 1:1:25
-%     figure
-%     plot(tsim, ynonlineaire(:,i));title(titre(i));
-% end
-%trajectoires
 
 % Comparaison des tension Va, Vb, Vc
 figure
@@ -51,11 +45,13 @@ plot(tsim, ynonlineaire(:,24));
 plot(tsim, ynonlineaire(:,25));
 plot(tsim, ySystemeNonLineaire(:,17));
 plot(tsim, ySystemeNonLineaire(:,18));
-plot(tsim, ySystemeNonLineaire(:,19)); legend('Va prof', 'Vb prof', 'Vc prof', 'Va', 'Vb', 'Vc');grid on; grid minor;
+plot(tsim, ySystemeNonLineaire(:,19));
+legend('Va prof', 'Vb prof', 'Vc prof', 'Va', 'Vb', 'Vc');grid on; grid minor;
 subplot(212);
 plot(tsim, ynonlineaire(:,23) - ySystemeNonLineaire(:,17)); hold on; title('Différence des tension Va Vb Vc');
 plot(tsim, ynonlineaire(:,24) - ySystemeNonLineaire(:,18));
-plot(tsim, ynonlineaire(:,25) - ySystemeNonLineaire(:,19));legend('Diff Va', 'Diff Vb', 'Diff Vc');grid on; grid minor;
+plot(tsim, ynonlineaire(:,25) - ySystemeNonLineaire(:,19));
+legend('Diff Va', 'Diff Vb', 'Diff Vc');grid on; grid minor;
 
 % Comparaison des courants Ia, Ib, Ic
 figure
@@ -65,11 +61,13 @@ plot(tsim, ynonlineaire(:,12));
 plot(tsim, ynonlineaire(:,13));
 plot(tsim, ySystemeNonLineaire(:,5));
 plot(tsim, ySystemeNonLineaire(:,6));
-plot(tsim, ySystemeNonLineaire(:,7)); legend('Ia prof', 'Ib prof', 'Ic prof', 'Ia', 'Ib', 'Ic');grid on; grid minor;
+plot(tsim, ySystemeNonLineaire(:,7));
+legend('Ia prof', 'Ib prof', 'Ic prof', 'Ia', 'Ib', 'Ic');grid on; grid minor;
 subplot(212); 
 plot(tsim, ySystemeNonLineaire(:,5) - ynonlineaire(:,11)); hold on;title('Différence des courants Ia Ib Ic');
 plot(tsim, ySystemeNonLineaire(:,6) - ynonlineaire(:,12));
-plot(tsim, ySystemeNonLineaire(:,7) - ynonlineaire(:,13));legend('Diff Ia', 'Diff Ib', 'Diff Ic');grid on; grid minor;
+plot(tsim, ySystemeNonLineaire(:,7) - ynonlineaire(:,13));
+legend('Diff Ia', 'Diff Ib', 'Diff Ic');grid on; grid minor;
 
 % Comparaison des Forces Fa, Fb, Fc
 figure
@@ -79,12 +77,14 @@ plot(tsim, ynonlineaire(:,21));
 plot(tsim, ynonlineaire(:,22));
 plot(tsim, ySystemeNonLineaire(:,14));
 plot(tsim, ySystemeNonLineaire(:,15));
-plot(tsim, ySystemeNonLineaire(:,16)); legend('Fa prof', 'Fb prof', 'Fc prof', 'Fa', 'Fb', 'Fc');grid on; grid minor;
+plot(tsim, ySystemeNonLineaire(:,16));
+legend('Fa prof', 'Fb prof', 'Fc prof', 'Fa', 'Fb', 'Fc');grid on; grid minor;
 ylim([-5 5]);
 subplot(212); 
 plot(tsim, ySystemeNonLineaire(:,14) - ynonlineaire(:,20)); hold on;title('Différence des forces Fa Fb Fc');
 plot(tsim, ySystemeNonLineaire(:,15) - ynonlineaire(:,21));
-plot(tsim, ySystemeNonLineaire(:,16) - ynonlineaire(:,22));legend('Diff Fa', 'Diff Fb', 'Diff Fc');grid on; grid minor;
+plot(tsim, ySystemeNonLineaire(:,16) - ynonlineaire(:,22));
+legend('Diff Fa', 'Diff Fb', 'Diff Fc');grid on; grid minor;
 
 % Comparaison des angles de la plaque phi et theta
 figure
@@ -118,3 +118,31 @@ plot(tsim, ySystemeNonLineaire(:,4) - ynonlineaire(:,9));
 plot(tsim, ySystemeNonLineaire(:,3) - ynonlineaire(:,10));
 legend('Diff Px', 'Diff Py', 'Diff Vx', 'Diff Vy');grid on; grid minor;
 ylim([-0.06 0.06])
+
+%% Comparaison des distances
+figure
+subplot(211); 
+plot(tsim, ynonlineaire(:,14), '--');
+hold on;title('Distance zA zB zC zD zE zF des deux modèles');
+plot(tsim, ynonlineaire(:,15), '--');
+plot(tsim, ynonlineaire(:,16), '--');
+plot(tsim, ynonlineaire(:,17), '--');
+plot(tsim, ynonlineaire(:,18), '--');
+plot(tsim, ynonlineaire(:,19), '--');
+plot(tsim, ySystemeNonLineaire(:,20));
+plot(tsim, ySystemeNonLineaire(:,21));
+plot(tsim, ySystemeNonLineaire(:,22));
+plot(tsim, ySystemeNonLineaire(:,23));
+plot(tsim, ySystemeNonLineaire(:,24));
+plot(tsim, ySystemeNonLineaire(:,25));
+legend('zA prof', 'zB prof', 'zC prof', 'zD prof', 'zE prof', 'zF prof', 'zA', 'zB', 'zC', 'zD', 'zE', 'zF');grid on; grid minor;
+subplot(212); 
+plot(tsim, ySystemeNonLineaire(:,20) - ynonlineaire(:,14)); hold on;title('Différence des distances zA zB zC zD zE zF');
+plot(tsim, ySystemeNonLineaire(:,21) - ynonlineaire(:,15));
+plot(tsim, ySystemeNonLineaire(:,22) - ynonlineaire(:,16));
+plot(tsim, ySystemeNonLineaire(:,23) - ynonlineaire(:,17));
+plot(tsim, ySystemeNonLineaire(:,24) - ynonlineaire(:,18));
+plot(tsim, ySystemeNonLineaire(:,25) - ynonlineaire(:,19));
+legend('Diff zA', 'Diff zB', 'Diff zC', 'Diff zD', 'Diff zE', 'Diff zF');grid on; grid minor;
+
+
