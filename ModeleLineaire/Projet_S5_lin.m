bf2bea115f1294eb0494a7d0e3d0acbcc2ccb36a
Natgord <natgourdeau@gmail.com>
2019-11-06 11:54:40 -0500

    Linéairisation fonctionelle et propre
    Rendre plus propre linéairisation

--- ModeleLineaire/Projet_S5_lin.m
+++ ModeleLineaire/Projet_S5_lin.m
@@ -1,48 +1,11 @@
 %%
 %Projet S5
 %Aymen Mousli
-% Linéraisation
+% LLinéraisation
 
-%%
-Rs = 3.9/1000;
-masseS = 8/1000;
-g = 9.81;
-Js = 2*masseS*Rs^2/5;
-
-rabc = 95.2e-03;
-bE1 = 13.029359254409743;
-g = 9.81;
-m = 0.442;
-R = 3.6;
-L = 0.115;
-
-Xa = rabc;
-Ya = 0;
-Za = 0;
-Xb = -rabc*sind(30);
-Yb = rabc*cosd(30);
-Zb = 0;
-Xc = -rabc*sind(30);
-Yc = -rabc*cosd(30);
-Zc = 0;
-
-XA = rabc;
-YA = 0;
-ZA = 0;
-XB = -rabc*sind(30);
-YB = rabc*cosd(30);
-ZB = 0;
-XC = -rabc*sind(30);
-YC = -rabc*cosd(30);
-ZC = 0;
-
-rdef = 80/1000;
-XD = rdef*sind(30);
-XE = -rdef;
-XF = rdef*sind(30);
-YD = rdef*cosd(30);
-YE = 0;
-YF = -rdef*cosd(30);
+%% Constantes
+m = mtot;
+RR = 3.6;
 
 aE0 = X1(1);
 aE1 = X1(2);
@@ -54,17 +17,12 @@ as1 = X3(2);
 as2 = X3(3);
 as3 = X3(4);
 
-Jpx = 347e-06
 
-Jpy = 347e-06
-
-
-%%
-
+%% ?????
 syms zo theta phi I Feq Deltaphi Deltatheta Deltazo deltaI
 
-FEA = (-I^2 + bE1*I)/(aE0 + aE1*(zo - Xa*theta + Ya*phi) + aE2*(zo - Xa*theta + Ya*phi)^2 + aE3*(zo - Xa*theta + Ya*phi)^3);
-FSA = -1/(as0 + as1*(zo - Xa*theta + Ya*phi) + as2*(zo - Xa*theta + Ya*phi)^2 + as3*(zo - Xa*theta + Ya*phi)^3);
+FEA = (-I^2 + bE1*I)/(aE0 + aE1*(zo - XA*theta + YA*phi) + aE2*(zo - XA*theta + YA*phi)^2 + aE3*(zo - XA*theta + YA*phi)^3);
+FSA = -1/(as0 + as1*(zo - XA*theta + YA*phi) + as2*(zo - XA*theta + YA*phi)^2 + as3*(zo - XA*theta + YA*phi)^3);
 FA = FEA+FSA;
 
 alphaA(phi,theta) = diff(FA,phi,1);
@@ -74,111 +32,103 @@ sigmaA = diff(FA,I,1);
 
 % DeltaFA = FAeq + alphaA(0,0) * Deltaphi + betaA(0,0)*Deltatheta + gammaA*Deltazo + sigmaA*deltaI
 
-%%
-%Condtions d'équilibre
+
+%% Condtions d'équilibre
 syms iAeq iBeq iCeq zoeq xs ys
 
 %Equilibre des forces
-
-FCeq(xs,ys) = -m*g*((Xa*Yb + Xb*ys - xs*Yb - ys*Xa)/(Yb*Xa + Xb*Yc - Xc*Yb -Yc*Xa))
-FAeq(xs,ys) = FCeq(0,0)*((Xb*Yc)/(Xa*Yb) - Xc/Xa) + m*g*((Xb*ys)/(Xa*Yb) - (xs*Yb)/(Xa*Yb))
-FBeq(xs,ys) = -( (Yc/Yb)*FCeq(0,0) + m*g*(ys/Yb))
+FCeq(xs,ys) = -m*g*((XA*YB + XB*ys - xs*YB - ys*XA)/(YB*XA + XB*YC - XC*YB -YC*XA));
+FAeq(xs,ys) = FCeq(0,0)*((XB*YC)/(XA*YB) - XC/XA) + m*g*((XB*ys)/(XA*YB) - (xs*YB)/(XA*YB));
+FBeq(xs,ys) = -( (YC/YB)*FCeq(0,0) + m*g*(ys/YB));
 
 %Equilibre des courants
-
 if FAeq(0,0) <0
-    fA(zoeq) = -iAeq^2 + bE1*iAeq - ( FAeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3))
+    fA(zoeq) = -iAeq^2 + bE1*iAeq - ( FAeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3));
     
-    Soluce_iAeq = vpasolve(fA(0.015)==0,iAeq)
+    Soluce_iAeq = vpasolve(fA(0.015)==0,iAeq);
     if Soluce_iAeq(1) <0 && imag(Soluce_iAeq(1))==0
-        Soluce_iAeqfin = Soluce_iAeq(1)
+        Soluce_iAeqfin = Soluce_iAeq(1);
     else
-        Soluce_iAeqfin = Soluce_iAeq(2)
+        Soluce_iAeqfin = Soluce_iAeq(2);
     end
 
 end
 
 if FAeq(0,0) >0
-    fA(zoeq) = iAeq^2 + bE1*iAeq - ( FAeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3))
+    fA(zoeq) = iAeq^2 + bE1*iAeq - ( FAeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3));
     
-    Soluce_iAeq = vpasolve(fA(0.015)==0,iAeq)
+    Soluce_iAeq = vpasolve(fA(0.015)==0,iAeq);
     if Soluce_iAeq(1) >0 && imag(Soluce_iAeq(1))==0
-        Soluce_iAeqfin = Soluce_iAeq(1)
+        Soluce_iAeqfin = Soluce_iAeq(1);
     else
-        Soluce_iAeqfin = Soluce_iAeq(2)
+        Soluce_iAeqfin = Soluce_iAeq(2);
     end
 
 end
 
 if FBeq(0,0) <0
-    fB(zoeq) = -iBeq^2 + bE1*iBeq - ( FBeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3))
+    fB(zoeq) = -iBeq^2 + bE1*iBeq - ( FBeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3));
 
-    Soluce_iBeq = vpasolve(fB(0.015)==0,iBeq)
+    Soluce_iBeq = vpasolve(fB(0.015)==0,iBeq);
     if Soluce_iBeq(1) <0 && imag(Soluce_iBeq(1))==0
-        Soluce_iBeqfin = Soluce_iBeq(1)
+        Soluce_iBeqfin = Soluce_iBeq(1);
     else
-        Soluce_iBeqfin = Soluce_iBeq(2)
+        Soluce_iBeqfin = Soluce_iBeq(2);
     end
 
 end
 
 if FBeq(0,0) >0
-    fB(zoeq) = iBeq^2 + bE1*iBeq - ( FBeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3))
+    fB(zoeq) = iBeq^2 + bE1*iBeq - ( FBeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3));
     
-    Soluce_iBeq = vpasolve(fB(0.015)==0,iBeq)
+    Soluce_iBeq = vpasolve(fB(0.015)==0,iBeq);
     if Soluce_iBeq(1) >0 && imag(Soluce_iBeq(1))==0
-        Soluce_iBeqfin = Soluce_iBeq(1)
+        Soluce_iBeqfin = Soluce_iBeq(1);
     else
-        Soluce_iBeqfin = Soluce_iBeq(2)
+        Soluce_iBeqfin = Soluce_iBeq(2);
     end
 
 end
 
 if FCeq(0,0) <0
-    fC(zoeq) = -iCeq^2 + bE1*iCeq - ( FCeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3))
+    fC(zoeq) = -iCeq^2 + bE1*iCeq - ( FCeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3));
 
-    Soluce_iCeq = vpasolve(fC(0.015)==0,iCeq)
+    Soluce_iCeq = vpasolve(fC(0.015)==0,iCeq);
     if Soluce_iCeq(1) <0 && imag(Soluce_iCeq(1))==0
-        Soluce_iCeqfin = Soluce_iCeq(1)
+        Soluce_iCeqfin = Soluce_iCeq(1);
     else
-        Soluce_iCeqfin = Soluce_iCeq(2)
+        Soluce_iCeqfin = Soluce_iCeq(2);
     end
 
 end
 
 if FCeq(0,0) >0
-   fC(zoeq) = iCeq^2 + bE1*iCeq - ( FCeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3))
+   fC(zoeq) = iCeq^2 + bE1*iCeq - ( FCeq(0,0)*(aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3) + (aE0 + aE1*zoeq + aE2*zoeq^2 + aE3*zoeq^3)/(as0 + as1*zoeq + as2*zoeq^2 + as3*zoeq^3));
 
-    Soluce_iCeq = vpasolve(fC(0.015)==0,iCeq)
+    Soluce_iCeq = vpasolve(fC(0.015)==0,iCeq);
     if Soluce_iCeq(1) >0 && imag(Soluce_iCeq(1))==0
-        Soluce_iCeqfin = Soluce_iCeq(1)
+        Soluce_iCeqfin = Soluce_iCeq(1);
     else
-        Soluce_iCeqfin = Soluce_iCeq(2)
+        Soluce_iCeqfin = Soluce_iCeq(2);
     end
 
 end
 
-%%
-%Equilibre des tensions 
+%Équilibre des tensions 
+VAeq = RR*Soluce_iAeqfin;
+VBeq = RR*Soluce_iBeqfin;
+VCeq = RR*Soluce_iCeqfin;
 
-VAeq = R*Soluce_iAeqfin
-VBeq = R*Soluce_iBeqfin
-VCeq = R*Soluce_iCeqfin
-
-%Equilibre des angles
-
+%Équilibre des angles
 Phieq = 0;
 Thetaeq =0;
 
-%Equilibre des vitesse et accélération nulle
 
-%%
-% Equation linéaire
-
+%% Équation linéaire
 syms zo theta phi Feq Deltaphi Deltatheta Deltazo deltaI Ia
 
-FEA(phi,theta,I,zo) = (-I^2 + bE1*I)/(aE0 + aE1*(zo - Xa*theta + Ya*phi) + aE2*(zo - Xa*theta + Ya*phi)^2 + aE3*(zo - Xa*theta + Ya*phi)^3);
-FSA(phi,theta,I,zo) = -1/(as0 + as1*(zo - Xa*theta + Ya*phi) + as2*(zo - Xa*theta + Ya*phi)^2 + as3*(zo - Xa*theta + Ya*phi)^3);
+FEA(phi,theta,I,zo) = (-I^2 + bE1*I)/(aE0 + aE1*(zo - XA*theta + YA*phi) + aE2*(zo - XA*theta + YA*phi)^2 + aE3*(zo - XA*theta + YA*phi)^3);
+FSA(phi,theta,I,zo) = -1/(as0 + as1*(zo - XA*theta + YA*phi) + as2*(zo - XA*theta + YA*phi)^2 + as3*(zo - XA*theta + YA*phi)^3);
 FA(phi,theta,I,zo) = FEA(phi,theta,I,zo)+FSA(phi,theta,I,zo);
 
 alphaA(phi,theta,I,zo) = diff(FA,phi,1);
@@ -186,8 +136,8 @@ betaA(phi,theta,I,zo) = diff(FA,theta,1);
 gammaA(phi,theta,I,zo) = diff(FA,zo,1);
 sigmaA(phi,theta,I,zo) = diff(FA,I,1);
 
-FEB(phi,theta,I,zo) = (-I^2 + bE1*I)/(aE0 + aE1*(zo - Xb*theta + Yb*phi) + aE2*(zo - Xb*theta + Yb*phi)^2 + aE3*(zo - Xb*theta + Yb*phi)^3);
-FSB(phi,theta,I,zo) = -1/(as0 + as1*(zo - Xb*theta + Yb*phi) + as2*(zo - Xb*theta + Yb*phi)^2 + as3*(zo - Xb*theta + Yb*phi)^3);
+FEB(phi,theta,I,zo) = (-I^2 + bE1*I)/(aE0 + aE1*(zo - XB*theta + YB*phi) + aE2*(zo - XB*theta + YB*phi)^2 + aE3*(zo - XB*theta + YB*phi)^3);
+FSB(phi,theta,I,zo) = -1/(as0 + as1*(zo - XB*theta + YB*phi) + as2*(zo - XB*theta + YB*phi)^2 + as3*(zo - XB*theta + YB*phi)^3);
 FB(phi,theta,I,zo) = FEB(phi,theta,I,zo)+FSB(phi,theta,I,zo);
 
 alphaB(phi,theta,I,zo) = diff(FB,phi,1);
@@ -195,8 +145,8 @@ betaB(phi,theta,I,zo) = diff(FB,theta,1);
 gammaB(phi,theta,I,zo) = diff(FB,zo,1);
 sigmaB(phi,theta,I,zo) = diff(FB,I,1);
 
-FEC(phi,theta,I,zo) = (-I^2 + bE1*I)/(aE0 + aE1*(zo - Xc*theta + Yc*phi) + aE2*(zo - Xc*theta + Yc*phi)^2 + aE3*(zo - Xc*theta + Yc*phi)^3);
-FSC(phi,theta,I,zo) = -1/(as0 + as1*(zo - Xc*theta + Yc*phi) + as2*(zo - Xc*theta + Yc*phi)^2 + as3*(zo - Xc*theta + Yc*phi)^3);
+FEC(phi,theta,I,zo) = (-I^2 + bE1*I)/(aE0 + aE1*(zo - XC*theta + YC*phi) + aE2*(zo - XC*theta + YC*phi)^2 + aE3*(zo - XC*theta + YC*phi)^3);
+FSC(phi,theta,I,zo) = -1/(as0 + as1*(zo - XC*theta + YC*phi) + as2*(zo - XC*theta + YC*phi)^2 + as3*(zo - XC*theta + YC*phi)^3);
 FC(phi,theta,I,zo) = FEC(phi,theta,I,zo)+FSC(phi,theta,I,zo);
 
 alphaC(phi,theta,I,zo) = diff(FC,phi,1);
@@ -204,8 +154,9 @@ betaC(phi,theta,I,zo) = diff(FC,theta,1);
 gammaC(phi,theta,I,zo) = diff(FC,zo,1);
 sigmaC(phi,theta,I,zo) = diff(FC,I,1);
 
-%%
 
+%% MATRICES
+%Matrices de zéros et identitées
 mat_One33 = eye(3);
 mat_One44 = eye(4);
 mat_One22 = eye(2);
@@ -217,28 +168,29 @@ mat_Zero34 = zeros(3,4);
 mat_Zero43 = zeros(4,3);
 mat_Zero73 = zeros(7,3);
 
-
-PP = [YB/Jpx*alphaB(0,0,Soluce_iBeqfin,0.015)+YC/Jpx*alphaC(0,0,Soluce_iCeqfin,0.015) YB/Jpx*betaB(0,0,Soluce_iBeqfin,0.015)+YC/Jpx*betaC(0,0,Soluce_iCeqfin,0.015) YB/Jpx*gammaB(0,0,Soluce_iBeqfin,0.015)+YC/Jpx*gammaC(0,0,Soluce_iCeqfin,0.015);
-      -XA/Jpy*alphaA(0,0,Soluce_iAeqfin,0.015)-XB/Jpy*alphaB(0,0,Soluce_iBeqfin,0.015)-XC/Jpy*alphaC(0,0,Soluce_iCeqfin,0.015) -XA/Jpy*betaA(0,0,Soluce_iAeqfin,0.015)-XB/Jpy*betaB(0,0,Soluce_iBeqfin,0.015)-XC/Jpy*betaC(0,0,Soluce_iCeqfin,0.015) -XA/Jpy*gammaA(0,0,Soluce_iAeqfin,0.015)-XB/Jpy*gammaB(0,0,Soluce_iBeqfin,0.015)-XC/Jpy*gammaC(0,0,Soluce_iCeqfin,0.015);
+%Matrices de linéairisation
+PP = [YB/Jx*alphaB(0,0,Soluce_iBeqfin,0.015)+YC/Jx*alphaC(0,0,Soluce_iCeqfin,0.015) YB/Jx*betaB(0,0,Soluce_iBeqfin,0.015)+YC/Jx*betaC(0,0,Soluce_iCeqfin,0.015) YB/Jx*gammaB(0,0,Soluce_iBeqfin,0.015)+YC/Jx*gammaC(0,0,Soluce_iCeqfin,0.015);
+      -XA/Jy*alphaA(0,0,Soluce_iAeqfin,0.015)-XB/Jy*alphaB(0,0,Soluce_iBeqfin,0.015)-XC/Jy*alphaC(0,0,Soluce_iCeqfin,0.015) -XA/Jy*betaA(0,0,Soluce_iAeqfin,0.015)-XB/Jy*betaB(0,0,Soluce_iBeqfin,0.015)-XC/Jy*betaC(0,0,Soluce_iCeqfin,0.015) -XA/Jy*gammaA(0,0,Soluce_iAeqfin,0.015)-XB/Jy*gammaB(0,0,Soluce_iBeqfin,0.015)-XC/Jy*gammaC(0,0,Soluce_iCeqfin,0.015);
       alphaA(0,0,Soluce_iAeqfin,0.015)/m+alphaB(0,0,Soluce_iBeqfin,0.015)/m+alphaC(0,0,Soluce_iCeqfin,0.015)/m betaA(0,0,Soluce_iAeqfin,0.015)/m+betaB(0,0,Soluce_iBeqfin,0.015)/m+betaC(0,0,Soluce_iCeqfin,0.015)/m gammaA(0,0,Soluce_iAeqfin,0.015)/m+gammaB(0,0,Soluce_iBeqfin,0.015)/m+gammaC(0,0,Soluce_iCeqfin,0.015)/m];
-PS = [0 masseS*g/Jpx;
-      -masseS*g/Jpy 0;
+PS = [0 mS*g/Jx;
+      -mS*g/Jy 0;
       0 0];
-PC = [0 YB/Jpx*sigmaB(0,0,Soluce_iBeqfin,0.015) YC/Jpx*sigmaC(0,0,Soluce_iCeqfin,0.015);
-      -XA/Jpx*sigmaA(0,0,Soluce_iAeqfin,0.015) -XB/Jpx*sigmaB(0,0,Soluce_iBeqfin,0.015) -XC/Jpx*sigmaC(0,0,Soluce_iCeqfin,0.015);
+PC = [0 YB/Jx*sigmaB(0,0,Soluce_iBeqfin,0.015) YC/Jx*sigmaC(0,0,Soluce_iCeqfin,0.015);
+      -XA/Jx*sigmaA(0,0,Soluce_iAeqfin,0.015) -XB/Jx*sigmaB(0,0,Soluce_iBeqfin,0.015) -XC/Jx*sigmaC(0,0,Soluce_iCeqfin,0.015);
       sigmaA(0,0,Soluce_iAeqfin,0.015)/m sigmaB(0,0,Soluce_iBeqfin,0.015)/m sigmaC(0,0,Soluce_iCeqfin,0.015)/m];
 SP = [0 -5/7*g 0;
       5/7*g 0 0];
-CC = [-R/L 0 0;
-      0 -R/L 0;
-      0 0 -R/L];
-CV = [1/L 0 0;
-      0 1/L 0;
-      0 0 1/L];
+CC = [-RR/LL 0 0;
+      0 -RR/LL 0;
+      0 0 -RR/LL];
+CV = [1/LL 0 0;
+      0 1/LL 0;
+      0 0 1/LL];
 Tdef = [YD -XD 1;
         YE -XE 1;
         YF -XF 1];
 
+%Matrices A,B,C et D du modèle linéaire
 A = [mat_Zero33 mat_One33 mat_Zero32 mat_Zero32 mat_Zero33;
      PP mat_Zero33 PS mat_Zero32 PC;
      mat_Zero23 mat_Zero23 mat_Zero22 mat_One22 mat_Zero23;
@@ -246,30 +198,13 @@ A = [mat_Zero33 mat_One33 mat_Zero32 mat_Zero32 mat_Zero33;
      mat_Zero33 mat_Zero33 mat_Zero32 mat_Zero32 CC];
 
 B = [mat_Zero33; mat_Zero33; mat_Zero23; mat_Zero23; CV];
+
 C = [Tdef mat_Zero33 mat_Zero34 mat_Zero33;
      mat_Zero43 mat_Zero43 mat_One44 mat_Zero43];
-D = [mat_Zero73];
+ 
+D = mat_Zero73;
 
 A = double(A);
 B = double(B);
 C = double(C);
 D = double(D);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- ModeleLineaire/Simulation/run_bancessai.m
+++ ModeleLineaire/Simulation/run_bancessai.m
@@ -22,6 +22,7 @@ tfin = 50;
 
 %initialisation
 bancEssaiConstantes
+
 %bancessai_ini  %faites tous vos calculs de modele ici
 run('../../Identification/Projet_s5.m');close all;
 run('../Projet_S5_lin.m');
@@ -34,46 +35,47 @@ open_system('DYNctl_ver4_etud_obfusc')
 set_param('DYNctl_ver4_etud_obfusc','AlgebraicLoopSolver','TrustRegion')
 sim('DYNctl_ver4_etud_obfusc')
 
+
 %% affichage
-
-% Comparaison de la position de la bille sur la plaque Px et Py
+% Comparaison de la position et de la vitesse de la bille
 figure
 subplot(211); 
-plot(tsim, ynonlineaire(:,7), '--');hold on;title('Position Px Py Vx Vy nonlinéaire (pointillé) vs Linéaire (ligne pleine)');
-plot(tsim, ynonlineaire(:,8), '--');
-plot(tsim, ynonlineaire(:,9), '--');
-plot(tsim, ynonlineaire(:,10), '--');
-plot(tsim, yLineaire.Data(:,4));
-plot(tsim, yLineaire.Data(:,5));
-plot(tsim, yLineaire.Data(:,6));
-plot(tsim, yLineaire.Data(:,7));
-legend('Px prof', 'Py prof', 'Vx prof', 'Vy prof', 'Px', 'Py', 'Vx', 'Vy');grid on; grid minor;
+hold on;
+plot(tsim, ynonlineaire(:,7), 'b--', tsim, ynonlineaire(:,8), 'r--', 'LineWidth', 2); %Positions Non Linéaire
+plot(tsim, ynonlineaire(:,9), 'g--', tsim, ynonlineaire(:,10), 'y--', 'LineWidth', 2); %Vitesses Non Linéaire
+plot(tsim, yLineaire.Data(:,4), 'b', tsim, yLineaire.Data(:,5), 'r'); %Position Linéaire
+plot(tsim, yLineaire.Data(:,6), 'g', tsim, yLineaire.Data(:,7), 'y'); %Vitesse Linéaire
+legend('Px (NL)', 'Py (NL)', 'Vx (NL)', 'Vy (NL)', 'Px (L)', 'Py (L)', 'Vx (L)', 'Vy (L)');grid on; grid minor;
 ylim([-0.06 0.06]);
-subplot(212); 
-plot(tsim, yLineaire.Data(:,4) - ynonlineaire(:,7)); hold on;title('Différence de la position Px Py et la vitesse Vx Vy');
-plot(tsim, yLineaire.Data(:,5) - ynonlineaire(:,8));
-plot(tsim, yLineaire.Data(:,6) - ynonlineaire(:,9));
-plot(tsim, yLineaire.Data(:,7) - ynonlineaire(:,10));
-legend('Diff Px', 'Diff Py', 'Diff Vx', 'Diff Vy');grid on; grid minor;
-ylim([-0.02 0.02])
+title('Position et vitesse Non Linéaire (pointillé) vs Linéaire (ligne pleine)');
 
-% Comparaison des distances
+subplot(212); 
+plot(tsim, yLineaire.Data(:,4) - ynonlineaire(:,7), 'b'); hold on; %Position x
+plot(tsim, yLineaire.Data(:,5) - ynonlineaire(:,8), 'r'); %Position y
+plot(tsim, yLineaire.Data(:,6) - ynonlineaire(:,9), 'g'); %Vitesse x
+plot(tsim, yLineaire.Data(:,7) - ynonlineaire(:,10), 'y'); %Vitesse y
+legend('\Delta Px', '\Delta Py', '\Delta Vx', '\Delta Vy');grid on; grid minor;
+ylim([-0.02 0.02])
+title('Différence entre Non Linéaire et Linéaire');
+hold off;
+
+% Comparaison des distances des capteurs
 figure
 subplot(211); 
-hold on;title('Distance zD zE zF des deux modèles');
-plot(tsim, ynonlineaire(:,17), '--');
-plot(tsim, ynonlineaire(:,18), '--');
-plot(tsim, ynonlineaire(:,19), '--');
-plot(tsim, yLineaire.Data(:,1));
-plot(tsim, yLineaire.Data(:,2));
-plot(tsim, yLineaire.Data(:,3));
-ylim([0 0.03])
-legend('zD prof', 'zE prof', 'zF prof', 'zD', 'zE', 'zF');grid on; grid minor;
+hold on;title('Distance ZD ZE ZF Non Linéaire (Pointillé) vs Linéaire (Ligne pleine)');
+plot(tsim, ynonlineaire(:,17), 'b--', 'LineWidth', 2);
+plot(tsim, ynonlineaire(:,18), 'r--', 'LineWidth', 2);
+plot(tsim, ynonlineaire(:,19), 'g--', 'LineWidth', 2);
+plot(tsim, yLineaire.Data(:,1), 'b');
+plot(tsim, yLineaire.Data(:,2), 'r');
+plot(tsim, yLineaire.Data(:,3), 'g');
+ylim([0 0.025])
+legend('ZD (NL)', 'ZE (NL)', 'ZF (NL)', 'ZD (L)', 'ZE (L)', 'ZF (L)');grid on; grid minor;
 subplot(212); 
-hold on;title('Différence des distances zA zB zC zD zE zF');
-plot(tsim, yLineaire.Data(:,1) - ynonlineaire(:,17));
-plot(tsim, yLineaire.Data(:,2) - ynonlineaire(:,18));
-plot(tsim, yLineaire.Data(:,3) - ynonlineaire(:,19));
-legend('Diff zD', 'Diff zE', 'Diff zF');grid on; grid minor;
-ylim([-0.02 0.02])
+hold on;title('Différence des distances ZA ZB ZC ZD ZE ZF');
+plot(tsim, yLineaire.Data(:,1) - ynonlineaire(:,17), 'b');
+plot(tsim, yLineaire.Data(:,2) - ynonlineaire(:,18), 'r');
+plot(tsim, yLineaire.Data(:,3) - ynonlineaire(:,19), 'g');
+legend('\Delta ZD', '\Delta ZE', '\Delta ZF');grid on; grid minor;
+ylim([-0.01 0.01])
 
