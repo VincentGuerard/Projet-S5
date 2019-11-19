1ed4e8a8d357b4ee887180ece5e783c9f62e8a67
Natgord <natgourdeau@gmail.com>
2019-11-19 12:12:43 -0500

    Update Calcul_Compensateurs.m
    Nouveau dossier pour compensateur calcul

--- ModeleDecouple/Simulation/Calcul_Compensateurs.m
+++ ModeleDecouple/Simulation/Calcul_Compensateurs.m
@@ -18,7 +18,7 @@ zeta_des_s = 0.9;
 
 
 %% CONCEPTION COMPENSATEURS
-%Inclinaision de la plaque (phi et theta)
+%*****Inclinaision de la plaque (phi et theta)*****%
 phi_des_angle = atan(-pi/log(Mp_des_angle)); %rad
 zeta_des_angle = cos(phi_des_angle);
 wn1_des_angle = pi/(Tp_des_angle*sqrt(1-zeta_des_angle^2));
@@ -28,12 +28,10 @@ wn_des_angle = max([wn1_des_angle wn2_des_angle wn3_des_angle]);
 pole_des_angle = [-zeta_des_angle*wn_des_angle+wn_des_angle*sqrt(1-zeta_des_angle^2)*1i;
                   -zeta_des_angle*wn_des_angle-wn_des_angle*sqrt(1-zeta_des_angle^2)*1i];
 
-figure; hold on; plot(pole_des_angle, 'p'); rlocus(FTplaque(1,1));
-title('Lieu des racines \phi / V_\phi et pôles désirés')
-figure; hold on; plot(pole_des_angle, 'p'); rlocus(FTplaque(1,1));
+figure; hold on; plot(pole_des_angle, 'p'); rlocus(FTplaque(1,1));  %Les pôles désirés sont placés à gauche                                                                   
+title('Lieu des racines \phi / V_\phi et pôles désirés')            %des tracés dominant, donc un          
+figure; hold on; plot(pole_des_angle, 'p'); rlocus(FTplaque(1,1));  %compensateur est nécessaire
 title('Lieu des racines \theta / V_\theta et pôles désirés')
-%Les pôles désirés sont placés à gauche des tracés dominant, donc un
-%compensateur est nécessaire
 
 Ga_angle_phi = avPhase(pole_des_angle, FTplaque(1,1), 100*pi/180, 0);
 Ga_angle_theta = avPhase(pole_des_angle, FTplaque(2,2), 100*pi/180, 0); 
@@ -47,3 +45,11 @@ figure; hold on; rlocus(FTplaque_theta_comp); plot(pole_des_angle, 'p'); hold of
 title('Lieu des racines de \theta / V_\theta avec AvPh')
 
 %Vérification des performances
+
+%*****Hauteur de la plaque*****%
+figure; margin(FTplaque(3,3));  %Les specs ne sont pas atteintes, donc compensation
+K_des_z = abs(polyval(denZ_VZ, wg_des_z*1i)/polyval(numZ_VZ, wg_des_z*1i));
+figure; margin(FTplaque(3,3)*K_des_z);
+[GM,PM, wp, wg] = margin(FTplaque(3,3)*K_des_z);
+
+
